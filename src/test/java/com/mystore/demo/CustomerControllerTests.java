791565b17ee708c1a0f064e3c7bf1d57c8573a91
Julio <julioamato@yahoo.com>
2020-03-03 22:03:28 -0500

    Add unit test

--- src/test/java/com/mystore/demo/CustomerControllerTests.java
+++ src/test/java/com/mystore/demo/CustomerControllerTests.java
@@ -1,5 +1,6 @@
 package com.mystore.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mystore.demo.controller.CustomerController;
 import com.mystore.demo.dao.CustomerRepository;
 import com.mystore.demo.model.Customer;
@@ -10,13 +11,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,8 +40,26 @@ class CustomerControllerTests {
 				new Customer(2,"fn2","ln2","b"));
 
 		when(customerRepositoryMock.findAll()).thenReturn(customerList);
-		this.mockMvc.perform(get("/api/customers")).andDo(print()).andExpect(status().isOk())
+
+		this.mockMvc.perform(get("/api/customers"))
+				.andDo(print())
+				.andExpect(status().isOk())
 				.andExpect(content().json(customerList.toString()) );
+
+		verify(customerRepositoryMock, times(1)).findAll();
+		verifyNoMoreInteractions(customerRepositoryMock);
+	}
+
+	@Test
+	public void testGetEmptyCustomerList() throws Exception {
+
+		List<Customer> customerList = new ArrayList<>();
+
+		when(customerRepositoryMock.findAll()).thenReturn(customerList);
+
+		this.mockMvc.perform(get("/api/customers"))
+				.andDo(print())
+				.andExpect(status().isNoContent());
 
 		verify(customerRepositoryMock, times(1)).findAll();
 		verifyNoMoreInteractions(customerRepositoryMock);
@@ -53,12 +73,33 @@ class CustomerControllerTests {
 
 		when(customerRepositoryMock.findByCountry("a")).thenReturn(customerList);
 
-		this.mockMvc.perform(get("/api/customers/").param("country","a")).andDo(print()).andExpect(status().isOk())
+		this.mockMvc.perform(get("/api/customers/").param("country","a"))
+				.andDo(print())
+				.andExpect(status().isOk())
 				.andExpect(content().json(customerList.toString()) );
 
 		verify(customerRepositoryMock, times(1)).findByCountry("a");
 		verifyNoMoreInteractions(customerRepositoryMock);
 	}
+
+	@Test
+	public void testGetCustomerById() throws Exception {
+
+		Customer customer = new Customer(1,"fn","ln","a");
+
+		Optional<Customer> optionalCustomer = Optional.of(customer);
+
+		when(customerRepositoryMock.findById(1)).thenReturn(optionalCustomer);
+
+		this.mockMvc.perform(get("/api/customers/1"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().json(customer.toString()));
+
+		verify(customerRepositoryMock, times(1)).findById(1);
+		verifyNoMoreInteractions(customerRepositoryMock);
+	}
+
 
 	@Test
 	public void testCreateCustomer() throws Exception {
@@ -69,18 +110,59 @@ class CustomerControllerTests {
 
 		this.mockMvc.perform(post("/api/customers/")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content("{ " +
-						"    \"id\": 7, " +
-						"    \"firstname\": \"Test3\", " +
-						"    \"lastname\": \"LT4\", " +
-						"    \"country\": \"USA\" " +
-						"}")
+				.content(asJsonString(customer))
 				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isCreated())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andDo(print())
 				.andExpect(status().isCreated())
-				.andExpect(content().json(customer.toString()) );
+				.andExpect(content().json(customer.toString())) ;
+
 	}
 
+
+
+
+	@Test
+	public void testUpdateCustomer() throws Exception {
+
+		Customer customer = new Customer(1,"fn","ln","a");
+
+		Optional<Customer> optionalCustomer = Optional.of(customer);
+
+		when(customerRepositoryMock.findById(1)).thenReturn(optionalCustomer);
+
+		when(customerRepositoryMock.save(any(Customer.class))).thenReturn(customer);
+
+		this.mockMvc.perform(put("/api/customers/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(customer))
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().json(customer.toString()) );
+
+		verify(customerRepositoryMock, times(1)).findById(1);
+		verify(customerRepositoryMock, times(1)).save(customer);
+
+	}
+
+	@Test
+	public void testDeleteCustomerById() throws Exception {
+
+		this.mockMvc.perform(delete("/api/customers/1"))
+				.andDo(print())
+				.andExpect(status().isNoContent())
+				.andExpect(content().string(""));
+
+		verify(customerRepositoryMock, times(1)).deleteById(1);
+		verifyNoMoreInteractions(customerRepositoryMock);
+	}
+
+
+	private String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
